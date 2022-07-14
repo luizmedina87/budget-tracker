6933e4bc8171577f74be3663a315acf34666141d
Luiz Medina <luizmedina87@gmail.com>
2022-07-14 17:22:28 -0400

    added idb functionality

---
+++ public/js/idb.js
@@ -0,0 +1,58 @@
+let db;
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_cash_flow", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    uploadCashFlow();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_cash_flow"], "readwrite");
+  const cashFlowObjStore = transaction.objectStore("new_cash_flow");
+  cashFlowObjStore.add(record);
+}
+
+function uploadCashFlow() {
+  const transaction = db.transaction(["new_cash_flow"], "readwrite");
+  const cashFlowObjStore = transaction.objectStore("new_cash_flow");
+  const getAll = cashFlowObjStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(["new_cash_flow"], "readwrite");
+          const cashFlowObjStore = transaction.objectStore("new_cash_flow");
+          cashFlowObjStore.clear();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadCashFlow);
